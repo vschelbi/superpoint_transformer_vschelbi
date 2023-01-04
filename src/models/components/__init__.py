6ea90eae4b5b1d0ed35bad58e68b38eeafb56c9b
drprojects <dr91@wanadoo.fr>
2023-01-04 10:48:15 +0100

    new NodeMLP model to simply classify nodes based on their hadcrafted features

--- src/models/components/__init__.py
+++ src/models/components/__init__.py
@@ -5,3 +5,4 @@ LightningModule can add heads and train for different types of tasks.
 from .nest import *
 from .pointnet import *
 from .hpointnet import *
+from .mlp import *
---
+++ src/models/components/mlp.py
@@ -0,0 +1,52 @@
+from torch_scatter import scatter_max
+from torch import nn
+from src.data import NAG
+from src.nn import MLP, FastBatchNorm1d
+
+
+__all__ = ['NodeMLP']
+
+
+class NodeMLP(nn.Module):
+    """Simple MLP on the handcrafted features of the level-i in a NAG.
+    This is used as a baseline to test how expressive handcrafted
+    features are.
+    """
+
+    def __init__(
+            self, dims, level=0, activation=nn.LeakyReLU(),
+            norm=FastBatchNorm1d, momentum=0.1, drop=None):
+
+        super().__init__()
+
+        self.level = level
+        self.mlp = MLP(
+            dims, activation=activation, norm=norm, momentum=momentum,
+            drop=drop)
+
+    @property
+    def out_dim(self):
+        return self.mlp.out_dim
+
+    def forward(self, nag):
+        assert isinstance(nag, NAG)
+        assert nag.num_levels > self.level
+
+        # Compute node features from the handcrafted features
+        x = self.mlp(nag[self.level].x)
+
+        # If node level is 1, output level-1 features
+        if self.level == 1:
+            return x
+
+        # If node level is 0, max-pool to produce level-1 features
+        if self.level == 0:
+            return scatter_max(
+                x, nag[0].super_index, dim=0, dim_size=nag[1].num_nodes)
+
+        # If node level is larger than 1, distribute parent features to
+        # level-1 nodes
+        super_index = nag[1].super_index
+        for i in range(2, self.level):
+            super_index = nag[i].super_index[super_index]
+        return x[super_index]
