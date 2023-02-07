6504d72e969106785660786b8110b718803083c9
drprojects <dr91@wanadoo.fr>
2023-02-07 23:31:39 +0100

    updating code to make qk-norm by num neighbors a cleaner feature

--- src/nn/attention.py
+++ src/nn/attention.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch_scatter import scatter_sum
 from torch_geometric.utils import softmax
-from src.nn.mlp import FFN, mlp
+from src.nn.mlp import RPEFFN
 
 __all__ = ['SelfAttentionBlock']
 
@@ -18,24 +18,47 @@ class SelfAttentionBlock(nn.Module):
     :param in_dim:
     :param out_dim:
     :param qkv_bias:
+    :param qk_dim:
     :param qk_scale:
+    :param scale_qk_by_neigh:
     :param attn_drop:
     :param drop:
+    :param k_rpe:
+    :param q_rpe:
+    :param c_rpe:
+    :param v_rpe:
+    :param heads_share_rpe:
     """
 
     def __init__(
-            self, dim, num_heads=1, in_dim=None, out_dim=None, qkv_bias=True,
-            qk_scale=None, attn_drop=None, drop=None, k_rpe=False, q_rpe=False,
-            c_rpe=False, v_rpe=False):
+            self,
+            dim,
+            num_heads=1,
+            in_dim=None,
+            out_dim=None,
+            qkv_bias=True,
+            qk_dim=8,
+            qk_scale=None,
+            scale_qk_by_neigh=True,
+            attn_drop=None,
+            drop=None,
+            k_rpe=False,
+            q_rpe=False,
+            c_rpe=False,
+            v_rpe=False,
+            heads_share_rpe=False):
         super().__init__()
 
         assert dim % num_heads == 0, f"dim must be a multiple of num_heads"
 
         self.dim = dim
         self.num_heads = num_heads
+        self.qk_dim = qk_dim
         self.qk_scale = qk_scale or (dim // num_heads) ** -0.5
+        self.scale_qk_by_neigh = scale_qk_by_neigh
+        self.heads_share_rpe = heads_share_rpe
 
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.qkv = nn.Linear(dim, qk_dim * 2 * num_heads + dim, bias=qkv_bias)
 
         # TODO: define relative positional encoding parameters and
         #  trunacted-normal initialize them (see Swin-T implementation)
@@ -43,11 +66,25 @@ class SelfAttentionBlock(nn.Module):
         # TODO: k/q/v RPE, pos/edge attr/both RPE, MLP/vector attention,
         #  mlp on pos/learnable lookup table/FFN/learnable FFN...
 
-        self.k_rpe = FFN(14, out_dim=dim, activation=nn.LeakyReLU()) if k_rpe else None
-        self.q_rpe = FFN(14, out_dim=dim, activation=nn.LeakyReLU()) if q_rpe else None
+        # Build the RPE encoders, with the option of sharing weights
+        # across all heads
+        rpe_dim = qk_dim if heads_share_rpe else qk_dim * num_heads
 
-        # self.k_rpe = mlp([14, dim, dim], last_activation=False) if k_rpe else None
-        # self.q_rpe = mlp([14, dim, dim], last_activation=False) if q_rpe else None
+        if not isinstance(k_rpe, bool):
+            self.k_rpe = k_rpe
+        else:
+            self.k_rpe = RPEFFN(14, out_dim=rpe_dim) if k_rpe else None
+
+        if not isinstance(q_rpe, bool):
+            self.q_rpe = q_rpe
+        else:
+            self.q_rpe = RPEFFN(14, out_dim=rpe_dim) if q_rpe else None
+
+        if c_rpe:
+            raise NotImplementedError
+
+        if v_rpe:
+            raise NotImplementedError
 
         self.in_proj = nn.Linear(in_dim, dim) if in_dim is not None else None
         self.out_proj = nn.Linear(out_dim, dim) if out_dim is not None else None
@@ -74,30 +111,45 @@ class SelfAttentionBlock(nn.Module):
         N = x.shape[0]
         E = edge_index.shape[1]
         H = self.num_heads
+        D = self.qk_dim
 
         # Optional linear projection of features
         if self.in_proj is not None:
             x = self.in_proj(x)
 
         # Compute queries, keys and values
-        qkv = self.qkv(x).view(N, 3, self.num_heads, self.dim // self.num_heads)
+        # qkv = self.qkv(x).view(N, 3, self.num_heads, self.dim // self.num_heads)
+        qkv = self.qkv(x)
 
-        # Separate and expand queries, keys, values and indices to edge
-        # shape
-        # TODO: make sure edge_index is undirected ? has self-loops ?
+        # # Separate and expand queries, keys, values and indices to edge
+        # # shape
+        # s = edge_index[0]  # [E]
+        # t = edge_index[1]  # [E]
+        # q = qkv[s, 0]  # [E, H, C // H]
+        # k = qkv[t, 1]  # [E, H, C // H]
+        # v = qkv[t, 2]  # [E, H, C // H]
+
+        # Separate queries, keys, values
+        DH = D * H
+        q = qkv[:, :DH].view(N, H, D)        # [N, H, D]
+        k = qkv[:, DH:2 * DH].view(N, H, D)  # [N, H, D]
+        v = qkv[:, 2 * DH:].view(N, H, -1)   # [N, H, C // H]
+
+        # Expand queries, keys and values to edges
         s = edge_index[0]  # [E]
         t = edge_index[1]  # [E]
-        q = qkv[s, 0]  # [E, H, C // H]
-        k = qkv[t, 1]  # [E, H, C // H]
-        v = qkv[t, 2]  # [E, H, C // H]
+        q = q[s]  # [E, H, D]
+        k = k[t]  # [E, H, D]
+        v = v[t]  # [E, H, C // H]
 
         # Apply scaling on the queries
         q = q * self.qk_scale
 
         # Apply scaling based on the number of neighbors of each node.
         # This will induce a scaled softmax
-        num_neigh_scale = (s.bincount(minlength=N) ** -0.5)[s].view(-1, 1, 1)
-        q = q * num_neigh_scale
+        if self.scale_qk_by_neigh:
+            num_neigh_scale = (s.bincount(minlength=N) ** -0.5)[s]
+            q = q * num_neigh_scale.view(-1, 1, 1)
 
         # TODO: add the relative positional encodings to the
         #  compatibilities here
@@ -107,9 +159,22 @@ class SelfAttentionBlock(nn.Module):
         #  - mlp (L-LN-A-L), learnable lookup table (see Stratified Transformer)
         #  - scalar rpe, vector rpe (see Stratified Transformer)
         if self.k_rpe is not None:
-            k = k + self.k_rpe(edge_attr).view(E, H, -1)
+            rpe = self.k_rpe(edge_attr)
+
+            # Expand RPE to all heads if heads share the RPE encoder
+            if self.heads_share_rpe:
+                rpe = rpe.repeat(1, H)
+
+            k = k + rpe.view(E, H, -1)
+
         if self.q_rpe is not None:
-            q = q + self.q_rpe(edge_attr).view(E, H, -1)
+            rpe = self.q_rpe(edge_attr)
+
+            # Expand RPE to all heads if heads share the RPE encoder
+            if self.heads_share_rpe:
+                rpe = rpe.repeat(1, H)
+
+            q = q + rpe.view(E, H, -1)
 
         # Compute compatibility scores from the query-key products
         compat = torch.einsum('ehd, ehd -> eh', q, k)  # [E, H]
