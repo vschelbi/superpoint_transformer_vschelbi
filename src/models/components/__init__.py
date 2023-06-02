2929597a7c158b717ad5f094f691a865fce0550c
drprojects <dr91@wanadoo.fr>
2023-06-02 15:08:17 +0200

    removing obsolete hpointnet and pointnet models

--- src/models/components/__init__.py
+++ src/models/components/__init__.py
@@ -3,6 +3,4 @@ LightningModule. These are complex architectures, on which a
 LightningModule can add heads and train for different types of tasks.
 """
 from .spt import *
-from .pointnet import *
-from .hpointnet import *
 from .mlp import *
